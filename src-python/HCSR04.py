0814e629bb8499ab334766d764aba40ba98355f2
hashtaglifesux <131010413+hashtaglifesux@users.noreply.github.com>
2025-01-31 17:40:45 +0400

    Add files via upload

--- src-python/HCSR04.py
+++ src-python/HCSR04.py
@@ -2,22 +2,26 @@ import RPi.GPIO as gpio
 from time import time, sleep
 
 # Device pinout - GPIO assignment 
-trig = 22
-echo = 27
-
-mach = 343 # speed of sound in m/s
+trig = 27
+echo = 22
 
 gpio.setmode(gpio.BCM)
 gpio.setup(trig, gpio.OUT)
 gpio.setup(echo, gpio.IN)
 
 # returns the distance in meters
-def measure():
+# mach = speed of sound in m/s
+def measure(mach: int = 343):
     gpio.output(trig, True)
-    start = time.time()
-    sleep(0.00004) # minimum is 0.00001
+    sleep(0.00001) # minimum is 0.00001
     gpio.output(trig, False)
     while not gpio.input(echo):
          pass
-    end = time.time()
-    return (end-start) * mach / 2
+    start = time()
+    while gpio.input(echo):
+         pass
+    end = time()
+    return (end-start)*mach/2
+
+
+# vt = d
