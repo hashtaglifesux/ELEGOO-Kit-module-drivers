ce5705d326ae9cd46f1e22f4585c9daabdc44ce7
hashtaglifesux <131010413+hashtaglifesux@users.noreply.github.com>
2025-02-06 08:15:37 +0400

    Update lightbyte.h

--- src-arduino/lightbyte.h
+++ src-arduino/lightbyte.h
@@ -1,12 +1,13 @@
+// display an integer value in binary using LEDs
 // pins used for each LED, MSB first
 int pinout[] = {31, 33, 35, 37, 39, 41, 43, 45};
 
-void binary(byte val) {
+void binary(byte value) {
   bool eightbits[] = {0, 0, 0, 0, 0 ,0 ,0, 0};
   for (int i = 7; i != -1; i--) {
     int place = 1 << i;
-    if (val >= place) {
-      val -= place;
+    if (value >= place) {
+      value -= place;
       eightbits[7-i] = 1;
     }
     else {
