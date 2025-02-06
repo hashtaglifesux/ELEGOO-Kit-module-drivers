a2bb37bca7509178e4d908ef463fdb3dcac9b5d9
hashtaglifesux <131010413+hashtaglifesux@users.noreply.github.com>
2025-02-06 13:47:16 +0400

    Add files via upload

---
+++ src-arduino/HCSR04.ino
@@ -0,0 +1,25 @@
+// device pinout - GPIO assignment
+byte trig;
+byte echo;
+
+void setup() {
+  pinMode(trig, OUTPUT);
+  pinMode(echo, OUTPUT);
+}
+
+// returns the distance in meters
+// mach = speed of sound in m/s
+float measure(int mach = 343) {
+  digitalWrite(trig, HIGH);
+  delay(0.01); // minimum is 0.01
+  digitalWrite(trig, LOW);
+  while (not digitalRead(echo)) {
+  }
+  unsigned long StartTime = millis();
+  while (digitalRead(echo)) {
+  }
+  unsigned long EndTime = millis();
+  return (EndTime-StartTime)*mach/2;
+}
+
+void loop() {}
