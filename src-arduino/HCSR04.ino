e928679fda0e7d0da44b7b1849fa68e37d6e3117
hashtaglifesux <131010413+hashtaglifesux@users.noreply.github.com>
2025-02-08 14:52:26 +0400

    Update HCSR04.ino

--- src-arduino/HCSR04.ino
+++ src-arduino/HCSR04.ino
@@ -1,25 +1,33 @@
+#include "/Users/adaaspirations/Documents/Arduino/libraries/lightbyte.h"
 // device pinout - GPIO assignment
-byte trig;
-byte echo;
+byte trig = 50;
+byte echo = 46;
 
 // returns the distance in meters
 // mach = speed of sound in m/s
 float measure(int mach = 343) {
   digitalWrite(trig, HIGH);
-  delay(0.01); // minimum is 0.01
+  delay(0.1); // minimum is 0.01
   digitalWrite(trig, LOW);
   while (not digitalRead(echo)) {
   }
-  unsigned long StartTime = millis();
+  float StartTime = (float)millis();
   while (digitalRead(echo)) {
   }
-  unsigned long EndTime = millis();
-  return (EndTime-StartTime)*mach/2000;
+  float EndTime = (float)millis();
+  return (EndTime-StartTime)* (float)mach/2000.0;
 }
 
 void setup() {
   pinMode(trig, OUTPUT);
-  pinMode(echo, OUTPUT);
+  pinMode(echo, INPUT);
+  for (int j = 0; j != 8; j++) {
+    pinMode(pinout[j], OUTPUT);
+  }
 }
 
-void loop() {}
+void loop() {
+  floatbyte(measure(), 5);
+  delay(500);
+  intbyte(0);
+}
