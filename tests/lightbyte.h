898ce12bed5c6b2f264d07926269303878291f81
hashtaglifesux <131010413+hashtaglifesux@users.noreply.github.com>
2025-02-10 18:14:50 +0400

    Update lightbyte.h

--- tests/lightbyte.h
+++ tests/lightbyte.h
@@ -1,20 +1,54 @@
-// display an integer value in binary using LEDs
-// pins used for each LED, MSB first
-byte pinout[] = {31, 33, 35, 37, 39, 41, 43, 45};
+// use 8 LEDs to display an integer or floating-point value in binary
 
-void binary(byte value) {
+int pinout[8];
+
+// configure the pins used for each LED, MSB first
+void lightbyte_init(int pins[8]) {
+  for (int i = 0; i!=8; i++) {
+    pinout[i] = pins[i];
+    pinMode(pinout[i], OUTPUT);
+  }
+}
+
+// convert an integer to binary
+void intbyte(byte val) {
   bool eightbits[] = {0, 0, 0, 0, 0 ,0 ,0, 0};
-  for (byte i = 7; i != -1; i--) {
-    byte place = 1 << i;
-    if (value >= place) {
-      value -= place;
+  for (int i = 7; i != -1; i--) {
+    int place = 1 << i;
+    if (val >= place) {
+      val -= place;
       eightbits[7-i] = 1;
     }
     else {
         eightbits[7-i] = 0;
     }
-    for (byte j = 0; j != 8; j++) {
+    for (int j = 0; j != 8; j++) {
         digitalWrite(pinout[j], eightbits[j]);
     }
   }
+}
+
+// convert a float to binary- 'precision' specifies how many bits will 
+// be reserved for the non-integer part
+void floatbyte(float value, byte precision) {
+    byte val =  (byte)(value * (2 << precision-1));
+    bool eightbits[] = {0, 0, 0, 0, 0, 0, 0, 0};
+    for (int i = 7; i != -1; i--) {
+        byte place = 1 << i;
+        if (val >= place) {
+            val -= place;
+            eightbits[7-i] = 1;
+        }
+        else {
+            eightbits[7-i] = 0;
+        }
+    }
+    // integer part
+    for (int j = 0; j != 8-precision; j++) {
+        digitalWrite(pinout[j], eightbits[j]);
+    }
+    // non-integer part
+    for (int j = 8-precision; j != 8; j++) {
+        digitalWrite(pinout[j], eightbits[j]);
+    }
 }
